823c0582d015a771b7664599191045bfdf2d255e
Oscar Esteban <code@oscaresteban.es>
2025-05-12 17:14:26 -1000

    fix: the commandline is set ``--model <undefined>``

--- nipreps/synthstrip/wrappers/nipype.py
+++ nipreps/synthstrip/wrappers/nipype.py
@@ -37,8 +37,12 @@ from nipype.interfaces.base import (
 _fs_home = os.getenv('FREESURFER_HOME', None)
 _default_model_path = Path(_fs_home) / 'models' / 'synthstrip.1.pt' if _fs_home else Undefined
 
+use_defaultmodel = False
 if _fs_home and not _default_model_path.exists():
     _default_model_path = Undefined
+else:
+    use_defaultmodel = True
+    _default_model_path = str(_default_model_path)
 
 
 class _SynthStripInputSpec(CommandLineInputSpec):
@@ -50,8 +54,8 @@ class _SynthStripInputSpec(CommandLineInputSpec):
     )
     use_gpu = traits.Bool(False, usedefault=True, argstr='-g', desc='Use GPU', nohash=True)
     model = File(
-        str(_default_model_path),
-        usedefault=True,
+        _default_model_path,
+        usedefault=use_defaultmodel,
         exists=True,
         argstr='--model %s',
         desc="file containing model's weights",
